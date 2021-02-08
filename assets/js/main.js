197e3b9f7e694b74663d118111dde401641c41e2
yousefsaqoul <76205108+yousefsaqoul@users.noreply.github.com>
2021-02-08 14:40:45 +0100

    js-data-type

---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+let left = document.getElementById("left")
+let center = document.getElementById("center")
+let right = document.getElementById("right")
+let test = document.getElementById("test")
+
+function myRight() {
+    test.classList.add("right")
+    test.classList.remove("center")
+
+}
+function myCenter() {
+    test.classList.add("center")
+    test.classList.remove("right")
+
+}
+console.log(Boolean(100))
+console.log(Boolean(3.14))
+console.log(Boolean(-15))
+console.log(Boolean('hello'))
+console.log(Boolean("false"))
+console.log(Boolean(1 + 7 + 3.14))
+console.log(Boolean(0))
+console.log(Boolean(-0))
+console.log(Boolean(NaN))
+console.log(Boolean(null))
+console.log(Boolean("false"))
+console.log(Boolean(true))
+console.log(Boolean(false))
+console.log(typeof (100))
+console.log(typeof ("100"))
+console.log(typeof ["bfhd"])
+console.log(8 == 5)
+
+let num = (5)
+let anotherNum = (8) 
+
+console.log (anotherNum == num)
+console.log (anotherNum > num)
+console.log (anotherNum < num)
+console.log (anotherNum >= num)
+console.log (anotherNum == "num")
+console.log (anotherNum != "8")
+let i = 1;
+let lastName = "saqoul";
+let j = "2";
+let status = "true";
+let hello = "hello";
+let helloworld = "hello";
+
+
+let z =10; 
+let x = 20;
+let y = 30;
+console.log (x+y);
+console.log ( y-x);
+console.log (x - y);
+console.log (x / y);
+console.log ((x * y)/z);
+
+resultOne = ((x*y)/z);
+console.log (resultOne)
+
+let a = 15;
+let b = 9;
+console.log (a % b)
+
+let c = 20;
+console.log ((a + b) * c)
+resultTwo = ((a + b)*c);
+ 
+console.log(++ a)
+console.log (--b)
+console.log (b-a)
+
+resultThree = (b-a);
+console.log(resultThree)
+
+console.log ( resultOne % resultTwo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
